6613a7986a2dc41f7e7565f5a43de7f7526a7911
Stephane Comeau <scomea@microsoft.com>
2022-03-30 12:38:48 -0700

    shift select

--- packages/web-components/fast-foundation/src/data-grid/data-grid.ts
+++ packages/web-components/fast-foundation/src/data-grid/data-grid.ts
@@ -401,7 +401,7 @@ export class FASTDataGrid extends FASTElement {
 
     private generatedGridTemplateColumns: string = "";
 
-    private lastSelectedRowIndex = -1;
+    private lastNotShiftSelectedRowIndex = -1;
     private lastShiftSelectedRowIndex = -1;
 
     constructor() {
@@ -645,9 +645,8 @@ export class FASTDataGrid extends FASTElement {
 
                 case "multiRow":
                     if (e.detail && (e.detail as MouseEvent | KeyboardEvent).shiftKey) {
-                        if (this.lastSelectedRowIndex === -1) {
+                        if (this.lastNotShiftSelectedRowIndex === -1) {
                             this.handleSingleRowSelection(changedRow);
-                            this.lastShiftSelectedRowIndex = changedRow.rowIndex;
                         } else {
                             let i: number;
                             let dirMod: number;
@@ -655,13 +654,13 @@ export class FASTDataGrid extends FASTElement {
                                 // undo the last thing
                                 dirMod =
                                     this.lastShiftSelectedRowIndex >
-                                    this.lastSelectedRowIndex
-                                        ? 1
-                                        : -1;
-                                i = this.lastShiftSelectedRowIndex + dirMod;
+                                    this.lastNotShiftSelectedRowIndex
+                                        ? -1
+                                        : 1;
+                                i = this.lastShiftSelectedRowIndex;
                                 for (
                                     i;
-                                    i !== this.lastShiftSelectedRowIndex;
+                                    i !== this.lastNotShiftSelectedRowIndex;
                                     i = i + dirMod
                                 ) {
                                     (this.rowElements[i] as DataGridRow).selected = false;
@@ -669,8 +668,10 @@ export class FASTDataGrid extends FASTElement {
                                 }
                             }
                             dirMod =
-                                changedRow.rowIndex > this.lastSelectedRowIndex ? 1 : -1;
-                            i = this.lastSelectedRowIndex + dirMod;
+                                changedRow.rowIndex > this.lastNotShiftSelectedRowIndex
+                                    ? 1
+                                    : -1;
+                            i = this.lastNotShiftSelectedRowIndex + dirMod;
                             for (i; i !== changedRow.rowIndex; i = i + dirMod) {
                                 (this.rowElements[i] as DataGridRow).selected =
                                     changedRow.selected;
@@ -702,6 +703,7 @@ export class FASTDataGrid extends FASTElement {
                                 }
                             }
                         }
+                        this.lastShiftSelectedRowIndex = changedRow.rowIndex;
                     } else if (
                         e.detail &&
                         (e.detail as MouseEvent | KeyboardEvent).ctrlKey
@@ -711,14 +713,14 @@ export class FASTDataGrid extends FASTElement {
                             !this.selectedRowIndexes.includes(changedRow.rowIndex)
                         ) {
                             this.selectedRowIndexes.push(changedRow.rowIndex);
-                            this.lastSelectedRowIndex = changedRow.rowIndex;
+                            this.lastNotShiftSelectedRowIndex = changedRow.rowIndex;
                         }
                         if (
                             !changedRow.selected &&
                             this.selectedRowIndexes.includes(changedRow.rowIndex)
                         ) {
                             this.selectedRowIndexes.splice(changedRow.rowIndex, 1);
-                            this.lastSelectedRowIndex = -1;
+                            this.lastNotShiftSelectedRowIndex = -1;
                         }
                         this.lastShiftSelectedRowIndex = -1;
                     } else {
@@ -732,7 +734,12 @@ export class FASTDataGrid extends FASTElement {
     }
 
     private selectAllRows(): void {
-        if (this.selectionMode !== "multiRow" && this.selectionMode !== "singleRow") {
+        if (this.selectionMode !== "multiRow") {
+            return;
+        }
+        if (this.selectedRowIndexes.length === this.rowElements.length) {
+            // deselect all if all are already selected
+            this.deselectAllRows();
             return;
         }
         this.selectedRowIndexes.splice(0);
@@ -740,7 +747,7 @@ export class FASTDataGrid extends FASTElement {
             this.selectedRowIndexes.push((element as DataGridRow).rowIndex);
             (element as DataGridRow).selected = true;
         });
-        this.lastSelectedRowIndex = -1;
+        this.lastNotShiftSelectedRowIndex = -1;
     }
 
     private deselectAllRows(): void {
@@ -748,7 +755,7 @@ export class FASTDataGrid extends FASTElement {
             (element as DataGridRow).selected = false;
         });
         this.selectedRowIndexes.splice(0);
-        this.lastSelectedRowIndex = -1;
+        this.lastNotShiftSelectedRowIndex = -1;
     }
 
     private handleSingleRowSelection(changedRow: DataGridRow): void {
@@ -760,10 +767,10 @@ export class FASTDataGrid extends FASTElement {
                 this.selectedRowIndexes.splice(0);
             }
             this.selectedRowIndexes.push(changedRow.rowIndex);
-            this.lastSelectedRowIndex = changedRow.rowIndex;
+            this.lastNotShiftSelectedRowIndex = changedRow.rowIndex;
         } else {
             this.selectedRowIndexes.splice(0);
-            this.lastSelectedRowIndex = -1;
+            this.lastNotShiftSelectedRowIndex = -1;
         }
     }
 
